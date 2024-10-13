26f5ebfc71146f42d840c022664f0cdb1c761392
KateCherednichok <chekavic@gmail.com>
2024-10-13 17:35:57 +0300

    edited emailController

--- src/main/java/ukma/speedyfix/controller/EmailController.java
+++ src/main/java/ukma/speedyfix/controller/EmailController.java
@@ -4,10 +4,12 @@ import com.example.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/emails")
@@ -17,10 +19,10 @@ public class EmailController {
     private EmailService emailService;
 
     @PostMapping("/send")
-    public ResponseEntity<String> sendEmail() {
+    public ResponseEntity<Boolean> sendEmail(@RequestBody Map<String, String> body) {
         try {
-            emailService.sendEmail("misha0106@gmail.com", "From SpeedyFix", "This is a test email.");
-            return ResponseEntity.ok("Email sent successfully to " + "chekavic@gmail.com");
+            emailService.sendEmail(body.get("to"), body.get("subject"), body.get("body"));
+            return ResponseEntity.ok(true);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
